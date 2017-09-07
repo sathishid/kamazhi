5129f5d31aa70c991a2e107854a2d60b817e4315
sathish <sathishbabur@hotmail.com>
2017-09-07 23:26:06 +0530

    Added the Android Studio projects.

---
+++ src/Kamazhi/app/src/main/java/com/sathishbabur/kamazhi_accounting/AddExpenseActivity.java
@@ -0,0 +1,78 @@
+package com.sathishbabur.kamazhi_accounting;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class AddExpenseActivity extends AppCompatActivity {
+
+    public static final int CATEGORY=1;
+    public static final int AMOUNT=2;
+    public static final int DATE =3;
+    public static final int COMMENTS=4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode==Activity.RESULT_OK) {
+            updateItem(requestCode,data);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    protected void updateItem(int requestCode,Intent intent)
+    {
+        TextView view;
+        switch (requestCode)
+        {
+            case CATEGORY:
+                view=(TextView)findViewById(R.id.txtCategory);
+                view.setText(intent.getStringExtra("result"));
+                break;
+            case AMOUNT:
+                view=(TextView)findViewById(R.id.txtAmount);
+                view.setText(intent.getStringExtra("result"));
+                break;
+            case DATE:
+                view=(TextView)findViewById(R.id.txtDate);
+                view.setText(intent.getStringExtra("result"));
+                break;
+            case COMMENTS:
+                view=(TextView)findViewById(R.id.txtCategory);
+                view.setText(intent.getStringExtra("result"));
+                break;
+
+        }
+    }
+
+    public void getCategory(View view) {
+        Intent intent=new Intent(this,HelperActivity.class);
+        intent.putExtra("type",HelperActivity.TEXT);
+        startActivityForResult(intent, CATEGORY);
+    }
+
+    public void getDate(View view) {
+        Intent intent=new Intent(this,HelperActivity.class);
+        intent.putExtra("type",HelperActivity.DATE);
+        startActivityForResult(intent, DATE);
+    }
+
+    public void getAmount(View view) {
+        Intent intent=new Intent(this,HelperActivity.class);
+        intent.putExtra("type",HelperActivity.NUMERIC);
+        startActivityForResult(intent, AMOUNT);
+    }
+
+    public void getComments(View view) {
+        Intent intent=new Intent(this,HelperActivity.class);
+        intent.putExtra("type",HelperActivity.TEXT);
+        startActivityForResult(intent, COMMENTS);
+    }
+}
---
+++ src/Kamazhi/app/src/main/java/com/sathishbabur/kamazhi_accounting/HelperActivity.java
@@ -0,0 +1,84 @@
+package com.sathishbabur.kamazhi_accounting;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.health.PackageHealthStats;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+
+public class HelperActivity extends AppCompatActivity {
+
+    public static final int NUMERIC=1;
+    public static final int DATE=2;
+    public static final int TEXT=3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helper);
+        addControl();
+    }
+    protected void addControl()
+    {
+        Intent requestIntent=this.getIntent();
+        int type=requestIntent.getIntExtra("type",TEXT);
+        switch (type)
+        {
+            case NUMERIC:
+                addNumeric();
+                break;
+            case TEXT:
+                addText();
+                break;
+            case DATE:
+                addDate();
+                break;
+        }
+    }
+
+    private void addDate() {
+        addEditText(InputType.TYPE_DATETIME_VARIATION_DATE);
+    }
+
+    private void addNumeric() {
+        addEditText(InputType.TYPE_CLASS_NUMBER);
+    }
+
+    protected void addText()
+    {
+        addEditText(InputType.TYPE_CLASS_TEXT);
+    }
+    EditText editText;
+    protected void addEditText(int inputType)
+    {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.llInput);
+        LinearLayout.LayoutParams gl=new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        editText=new EditText(this);
+        editText.setInputType(inputType);
+        layout.addView(editText, gl);
+    }
+    protected Intent getResultIntent()
+    {
+        Intent intent=new Intent();
+        intent.putExtra("result",editText.getText().toString());
+        return intent;
+    }
+
+    public void sendResultBack(View view) {
+        setResult(Activity.RESULT_OK,getResultIntent());
+        finish();
+    }
+}
