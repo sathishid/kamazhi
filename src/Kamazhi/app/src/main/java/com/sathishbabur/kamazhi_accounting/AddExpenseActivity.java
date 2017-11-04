3b15586ade1a38e9326a2b08cdf5cbff0085cad9
sathish <sathishbabur@hotmail.com>
2017-11-04 22:11:58 +0530

    Gradle project updated to new version.

--- src/Kamazhi/app/src/main/java/com/sathishbabur/kamazhi_accounting/AddExpenseActivity.java
+++ src/Kamazhi/app/src/main/java/com/sathishbabur/kamazhi_accounting/AddExpenseActivity.java
@@ -5,73 +5,103 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AddExpenseActivity extends AppCompatActivity {
 
-    public static final int CATEGORY=1;
-    public static final int AMOUNT=2;
-    public static final int DATE =3;
-    public static final int COMMENTS=4;
+    public static final int CATEGORY = 1;
+    public static final int AMOUNT = 2;
+    public static final int DATE = 3;
+    public static final int COMMENTS = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
+        updatePageType();
+    }
+
+    protected void updatePageType() {
+        int requestCode = getIntent().getIntExtra("requestCode", 1);
+        TextView txtTitle = (TextView) findViewById(R.id.txtTitle);
+        Button btnTransaction=(Button) findViewById(R.id.btnTransaction);
+        switch (requestCode) {
+            case HomeActivity.Expense:
+                txtTitle.setText(R.string.add_expense);
+                btnTransaction.setText(R.string.add_expense);
+                break;
+            case HomeActivity.Income:
+                txtTitle.setText(R.string.add_income);
+                btnTransaction.setText(R.string.add_income);
+                break;
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode==Activity.RESULT_OK) {
-            updateItem(requestCode,data);
+        if (resultCode == Activity.RESULT_OK) {
+            updateItem(requestCode, data);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
-    protected void updateItem(int requestCode,Intent intent)
-    {
+
+    protected void updateItem(int requestCode, Intent intent) {
         TextView view;
-        switch (requestCode)
-        {
+        switch (requestCode) {
             case CATEGORY:
-                view=(TextView)findViewById(R.id.txtCategory);
+                view = (TextView) findViewById(R.id.txtCategory);
                 view.setText(intent.getStringExtra("result"));
                 break;
             case AMOUNT:
-                view=(TextView)findViewById(R.id.txtAmount);
+                view = (TextView) findViewById(R.id.txtAmount);
                 view.setText(intent.getStringExtra("result"));
                 break;
             case DATE:
-                view=(TextView)findViewById(R.id.txtDate);
+                view = (TextView) findViewById(R.id.txtDate);
                 view.setText(intent.getStringExtra("result"));
                 break;
             case COMMENTS:
-                view=(TextView)findViewById(R.id.txtComments);
+                view = (TextView) findViewById(R.id.txtComments);
                 view.setText(intent.getStringExtra("result"));
                 break;
         }
     }
 
     public void getCategory(View view) {
-        Intent intent=new Intent(this,HelperActivity.class);
-        intent.putExtra("type",HelperActivity.TEXT);
+        Intent intent = new Intent(this, HelperActivity.class);
+        intent.putExtra("type", HelperActivity.TEXT);
         startActivityForResult(intent, CATEGORY);
     }
 
     public void getDate(View view) {
-        Intent intent=new Intent(this,DateHelperActivity.class);
-        intent.putExtra("type",HelperActivity.DATE);
+        Intent intent = new Intent(this, DateHelperActivity.class);
+        intent.putExtra("type", HelperActivity.DATE);
         startActivityForResult(intent, DATE);
     }
 
     public void getAmount(View view) {
-        Intent intent=new Intent(this,HelperActivity.class);
-        intent.putExtra("type",HelperActivity.NUMERIC);
+        Intent intent = new Intent(this, HelperActivity.class);
+        intent.putExtra("type", HelperActivity.NUMERIC);
         startActivityForResult(intent, AMOUNT);
     }
 
     public void getComments(View view) {
-        Intent intent=new Intent(this,HelperActivity.class);
-        intent.putExtra("type",HelperActivity.TEXT);
+        Intent intent = new Intent(this, HelperActivity.class);
+        intent.putExtra("type", HelperActivity.TEXT);
         startActivityForResult(intent, COMMENTS);
+    }
+
+    protected Intent getResultIntent() {
+        Intent intent = new Intent();
+        TextView txtView = (TextView) findViewById(R.id.txtAmount);
+        double dblValue = Double.parseDouble(txtView.getText().toString());
+        intent.putExtra("dblValue", dblValue);
+        return intent;
+    }
+
+    public void sendResultBack(View view) {
+        setResult(Activity.RESULT_OK, getResultIntent());
+        finish();
     }
 }
--- src/Kamazhi/app/src/main/java/com/sathishbabur/kamazhi_accounting/HomeActivity.java
+++ src/Kamazhi/app/src/main/java/com/sathishbabur/kamazhi_accounting/HomeActivity.java
@@ -1,21 +1,63 @@
 package com.sathishbabur.kamazhi_accounting;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.TextView;
+
+import com.sathishbabur.kamazhi_accounting.model.HomeViewModel;
 
 public class HomeActivity extends AppCompatActivity {
-
+    public static final int Expense=1;
+    public static final int Income=2;
+    HomeViewModel homeViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        homeViewModel=new HomeViewModel();
+        updateBalance();
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode== Activity.RESULT_OK) {
+            updateTranasction(requestCode,data);
+            updateBalance();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    public void updateTranasction(int requestCode,Intent data)
+    {
+        switch (requestCode)
+        {
+            case Expense:
+                homeViewModel.sub(data.getDoubleExtra("dblValue",0));
+                break;
+            case Income:
+                homeViewModel.sum(data.getDoubleExtra("dblValue",0));
+                break;
+        }
 
+    }
+    public void updateBalance()
+    {
+        TextView balanceView=(TextView) findViewById(R.id.txtBalance);
+        balanceView.setText(homeViewModel.getBalanceAmount()+"");
+    }
     public void addExpense(View view) {
-        Intent intent=new Intent(this,AddExpenseActivity.class);
-        startActivity(intent);
-
+        doTransaction(Expense);
     }
+    public void addIncome(View view)
+    {
+        doTransaction(Income);
+    }
+    public void doTransaction(int pageType){
+        Intent intent=new Intent(this,AddExpenseActivity.class);
+        intent.putExtra("requestCode",pageType);
+        startActivityForResult(intent,pageType);
+    }
+
 }
